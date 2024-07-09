e34d396076065224855e038a803b2812ba1dd8c2
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2024-07-09 16:05:41 +0300

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+   return (
+      <div className="app">
+         <header className="app-header py-6">
+            <h1>Book Library App</h1>
+         </header>
+      </div>
+   );
+}
+
+export default App;
